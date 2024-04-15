9aa93d0ba3fabb5c485bd710c3d4f10a0c6e3d06
Petrik van der Velde <petrikvandervelde@gmail.com>
2024-04-15 14:14:34 +1200

    Implemented input parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,365 @@
+use std::io::{self, BufRead};
+use std::num::{ParseFloatError, ParseIntError};
+use std::str::FromStr;
+use std::string::ToString;
+
+use strum::{Display, EnumString, ParseError};
+use thiserror::Error;
+
+#[cfg(test)]
+#[path = "tests.rs"]
+mod tests;
+
+/// Defines the different errors for the swerve model crate.
+#[derive(Debug, Error, PartialEq)]
+pub enum Error {
+    /// Indicates that one of the input strings didn't start in a valid way.
+    ///
+    /// * 'input' - The input string that was invalid.
+    #[error("The provided input string {input:?} was not valid. Expected the string to start with a number or one of [NATURAL, SHORT_TO_LONG, LONG_TO_SHORT].")]
+    InputStringShouldStartWithNumberOrKeyWord {
+        /// The input string that is incorrect
+        input: String,
+    },
+
+    /// Indicates that the current input string contains a pack information header, but the current line is not the first line. So duplicate information is
+    /// provided.
+    ///
+    /// * 'current_line' - The contents of the current line, which contains the duplicate header
+    /// * 'current_line_index' - The index of the current line.
+    #[error("The provided input string {current_line:?} contains pack information, but this line is not the first line of the input stream. It is the {current_line_index:?} line. This means there is duplicate header information.")]
+    InputContainsDuplicatePackInformation {
+        current_line: String,
+        current_line_index: usize,
+    },
+
+    /// Indicates that a string containing item information has too few or too many property values.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_count' - The number of property values that were found in the input string
+    #[error("The provided input string {input:?} contains too few or too many property values. Expecting 3 values, but got {property_count:?}")]
+    InvalidNumberOfPropertiesForPacks {
+        input: String,
+        property_count: usize,
+    },
+
+    // Indicates that a string containing pack information has an invalid value for the sort order.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_value' - The string containing the 'value' for the sort order
+    /// * 'source' - The source error
+    #[error("The provided input string {input:?} contains an invalid value for the sort order of a pack: {property_value:?}. Expected one of [NATURAL, SHORT_TO_LONG, LONG_TO_SHORT].")]
+    InvalidPackSortOrder {
+        input: String,
+        property_value: String,
+        #[source]
+        source: ParseError,
+    },
+
+    /// Indicates that a string containing pack information has an invalid value for the number of items in a pack.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_value' - The string containing the 'value' for the number of items
+    /// * 'source' - The source error
+    #[error("The provided input string {input:?} contains an invalid value for the number of the items in a pack: {property_value:?}. Expected a positive integer number.")]
+    InvalidPackItemCount {
+        input: String,
+        property_value: String,
+        #[source]
+        source: ParseIntError,
+    },
+
+    // Indicates that a string containing pack information has an invalid value for the weight of the pack.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_value' - The string containing the 'value' for the weight property
+    /// * 'source' - The source error
+    #[error("The provided input string {input:?} contains an invalid value for the weight of a pack: {property_value:?}. Expected a positive floating point number.")]
+    InvalidPackWeight {
+        input: String,
+        property_value: String,
+        #[source]
+        source: ParseFloatError,
+    },
+
+    /// Indicates that a string containing item information has too few or too many property values.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_count' - The number of property values that were found in the input string
+    #[error("The provided input string {input:?} contains too few or too many property values. Expecting 4 values, but got {property_count:?}")]
+    InvalidNumberOfPropertiesForItem {
+        input: String,
+        property_count: usize,
+    },
+
+    /// Indicates that a string containing item information has an invalid value for the length of the item.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_value' - The string containing the 'value' for the length property
+    /// * 'source' - The source error
+    #[error("The provided input string {input:?} contains an invalid value for the length of the item: {property_value:?}. Expected a positive floating point number.")]
+    InvalidItemLength {
+        input: String,
+        property_value: String,
+        #[source]
+        source: ParseFloatError,
+    },
+
+    /// Indicates that a string containing item information has an invalid value for the weight of the item.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_value' - The string containing the 'value' for the weight property
+    /// * 'source' - The source error
+    #[error("The provided input string {input:?} contains an invalid value for the weight of the item: {property_value:?}. Expected a positive floating point number.")]
+    InvalidItemWeight {
+        input: String,
+        property_value: String,
+        #[source]
+        source: ParseFloatError,
+    },
+
+    /// Indicates that a string containing item information has an invalid value for the number of items.
+    ///
+    /// * 'input' - The input string
+    /// * 'property_value' - The string containing the 'value' for the number of items
+    /// * 'source' - The source error
+    #[error("The provided input string {input:?} contains an invalid value for the number of the items: {property_value:?}. Expected a positive integer number.")]
+    InvalidItemCount {
+        input: String,
+        property_value: String,
+        #[source]
+        source: ParseIntError,
+    },
+}
+
+// Indices used when parsing the pack information from the input
+const PACK_SORT_ORDER_INDEX: usize = 0;
+const PACK_MAXIMUM_ITEM_COUNT_INDEX: usize = 1;
+const PACK_MAXIMUM_WEIGHT_INDEX: usize = 2;
+
+// Indices used when parsing the items from the input
+const ITEM_ID_INDEX: usize = 0;
+const ITEM_LENGTH_INDEX: usize = 1;
+const ITEM_QUANTITY_INDEX: usize = 2;
+const ITEM_WEIGHT_INDEX: usize = 3;
+
+/// Contains properties for an item and the number of items with these properties as provided in the input.
+struct ItemTemplate {
+    id: String,
+    length: f32,
+    weight: f32,
+    count: usize,
+}
+
+impl FromStr for ItemTemplate {
+    type Err = Error;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = line.split(',').collect();
+        if parts.len() != 4 {
+            return Err(Error::InvalidNumberOfPropertiesForItem {
+                input: line.to_string(),
+                property_count: parts.len(),
+            });
+        }
+
+        let id = parts[ITEM_ID_INDEX].to_string();
+
+        let mut length: f32 = 0.0;
+        match parts[ITEM_LENGTH_INDEX].parse() {
+            Ok(v) => length = v,
+            Err(e) => {
+                return Err(Error::InvalidItemLength {
+                    input: line.to_string(),
+                    property_value: parts[ITEM_LENGTH_INDEX].to_string(),
+                    source: e,
+                })
+            }
+        };
+
+        let mut weight = 0.0;
+        match parts[ITEM_WEIGHT_INDEX].parse() {
+            Ok(v) => weight = v,
+            Err(e) => {
+                return Err(Error::InvalidItemWeight {
+                    input: line.to_string(),
+                    property_value: parts[ITEM_WEIGHT_INDEX].to_string(),
+                    source: e,
+                })
+            }
+        };
+
+        let mut count = 0;
+        match parts[ITEM_QUANTITY_INDEX].parse() {
+            Ok(v) => count = v,
+            Err(e) => {
+                return Err(Error::InvalidItemCount {
+                    input: line.to_string(),
+                    property_value: parts[ITEM_QUANTITY_INDEX].to_string(),
+                    source: e,
+                })
+            }
+        };
+
+        Ok(Self {
+            id,
+            length,
+            weight,
+            count,
+        })
+    }
+}
+
+/// Defines the different ways in which packs can be ordered.
+#[derive(Clone, Copy, Debug, Display, EnumString, PartialEq)]
+enum PackSortOrder {
+    NotSet,
+    #[strum(to_string = "NATURAL")]
+    Natural,
+    #[strum(to_string = "SHORT_TO_LONG")]
+    ShortToLong,
+    #[strum(to_string = "LONG_TO_SHORT")]
+    LongToShort,
+}
+
+struct PackTemplate {
+    maximum_number_of_pieces: usize,
+    maximum_weight: f32,
+    sort_order: PackSortOrder,
+}
+
+impl PackTemplate {
+    fn new() -> PackTemplate {
+        PackTemplate {
+            maximum_number_of_pieces: 0,
+            maximum_weight: 0.0,
+            sort_order: PackSortOrder::NotSet,
+        }
+    }
+
+    fn from_line(&mut self, s: &str) -> Result<(), Error> {
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 3 {
+            return Err(Error::InvalidNumberOfPropertiesForPacks {
+                input: s.to_string(),
+                property_count: parts.len(),
+            });
+        }
+
+        let mut pack_sort_order = PackSortOrder::NotSet;
+        match PackSortOrder::from_str(parts[PACK_SORT_ORDER_INDEX]) {
+            Ok(s) => pack_sort_order = s,
+            Err(e) => {
+                return Err(Error::InvalidPackSortOrder {
+                    input: s.to_string(),
+                    property_value: parts[PACK_SORT_ORDER_INDEX].to_string(),
+                    source: e,
+                })
+            }
+        };
+
+        let mut maximum_number_of_items = 0;
+        match parts[PACK_MAXIMUM_ITEM_COUNT_INDEX].parse() {
+            Ok(v) => maximum_number_of_items = v,
+            Err(e) => {
+                return Err(Error::InvalidPackItemCount {
+                    input: s.to_string(),
+                    property_value: parts[PACK_MAXIMUM_ITEM_COUNT_INDEX].to_string(),
+                    source: e,
+                })
+            }
+        };
+
+        let mut maximum_weight = 0.0;
+        match parts[PACK_MAXIMUM_WEIGHT_INDEX].parse() {
+            Ok(v) => maximum_weight = v,
+            Err(e) => {
+                return Err(Error::InvalidPackWeight {
+                    input: s.to_string(),
+                    property_value: parts[PACK_MAXIMUM_WEIGHT_INDEX].to_string(),
+                    source: e,
+                })
+            }
+        };
+
+        self.maximum_number_of_pieces = maximum_number_of_items;
+        self.maximum_weight = maximum_weight;
+        self.sort_order = pack_sort_order;
+
+        Ok(())
+    }
+}
+
+struct Pack {
+    maximum_number_of_pieces: usize,
+    maximum_weight: f32,
+    //items: Vec<Item>,
+}
+
+impl Pack {
+    // Number of pieces
+    // Space left
+    // Weight left
+}
+
+fn parse_input<R: BufRead>(reader: &mut R) -> Result<(PackTemplate, Vec<ItemTemplate>), Error> {
+    let mut pack_template = PackTemplate::new();
+    let mut item_templates: Vec<ItemTemplate> = Vec::new();
+
+    let mut is_first_line = true;
+    let mut line_index: usize = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+
+        let trimmed_line = line.trim();
+
+        // The line should start either with an integer number or one of [NATURAL, SHORT_TO_LONG, LONG_TO_SHORT]
+        let is_number = if let Some(c) = trimmed_line.chars().next() {
+            c.is_digit(10)
+        } else {
+            false // Empty string
+        };
+
+        let is_keyword = trimmed_line.starts_with("NATURAL")
+            || trimmed_line.starts_with("SHORT_TO_LONG")
+            || trimmed_line.starts_with("LONG_TO_SHORT");
+
+        if !is_number && !is_keyword {
+            return Err(Error::InputStringShouldStartWithNumberOrKeyWord {
+                input: line.clone(),
+            });
+        }
+
+        // The first line contains information about the packs that we're allowed to create
+        if is_keyword {
+            if !is_first_line {
+                return Err(Error::InputContainsDuplicatePackInformation {
+                    current_line: line,
+                    current_line_index: line_index,
+                });
+            }
+
+            pack_template.from_line(trimmed_line)?;
+
+            // Any line after this cannot be the first line anymore.
+            is_first_line = false;
+        } else {
+            let item = ItemTemplate::from_str(trimmed_line)?;
+            item_templates.push(item);
+        }
+
+        line_index += 1;
+    }
+
+    Ok((pack_template, item_templates))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let (pack_template, item_templates) = parse_input(&mut stdin.lock()).expect("Parsing failure.");
+
+    // Now do something with the templates.
 }
---
+++ src/tests.rs
@@ -0,0 +1,208 @@
+use std::io::Cursor;
+
+use crate::*;
+
+// PackSortOrder
+
+#[test]
+fn when_parsing_sort_order_it_should_return_the_correct_type() {
+    let natural_sort_order =
+        PackSortOrder::from_str("NATURAL").expect("Failed to parse the natural sort order.");
+    assert_eq!(PackSortOrder::Natural, natural_sort_order);
+
+    let short_to_long_sort_order = PackSortOrder::from_str("SHORT_TO_LONG")
+        .expect("Failed to parse the short-to-long sort order.");
+    assert_eq!(PackSortOrder::ShortToLong, short_to_long_sort_order);
+
+    let long_to_short_sort_order = PackSortOrder::from_str("LONG_TO_SHORT")
+        .expect("Failed to parse the long-to-short sort order");
+    assert_eq!(PackSortOrder::LongToShort, long_to_short_sort_order);
+}
+
+#[test]
+fn when_creating_a_string_representation_of_the_sort_order_it_should_return_the_correct_value() {
+    assert_eq!("NATURAL", PackSortOrder::Natural.to_string());
+    assert_eq!("SHORT_TO_LONG", PackSortOrder::ShortToLong.to_string());
+    assert_eq!("LONG_TO_SHORT", PackSortOrder::LongToShort.to_string());
+}
+
+// ItemTemplate
+
+#[test]
+fn when_parsing_a_valid_item_input_string_it_should_return_an_item_template() {
+    let input = "item1,10.5,20,3.0";
+    let result = ItemTemplate::from_str(input);
+    assert!(result.is_ok());
+
+    let item = result.unwrap();
+    assert_eq!(item.id, "item1");
+    assert_eq!(item.length, 10.5);
+    assert_eq!(item.count, 20);
+    assert_eq!(item.weight, 3.0);
+}
+
+#[test]
+fn when_parsing_an_item_input_with_too_few_properties_it_should_return_an_error() {
+    let input = "item1,10.5,20";
+    let result = ItemTemplate::from_str(input);
+    assert!(result.is_err());
+    assert_eq!(
+        result.err().unwrap(),
+        Error::InvalidNumberOfPropertiesForItem {
+            input: input.to_string(),
+            property_count: 3
+        }
+    );
+}
+
+#[test]
+fn when_parsing_an_item_input_with_too_many_properties_it_should_return_an_error() {
+    let input = "item1,10.5,20,3.0,10.0";
+    let result = ItemTemplate::from_str(input);
+    assert!(result.is_err());
+    assert_eq!(
+        result.err().unwrap(),
+        Error::InvalidNumberOfPropertiesForItem {
+            input: input.to_string(),
+            property_count: 5
+        }
+    );
+}
+
+#[test]
+fn when_parsing_an_item_input_with_an_invalid_item_length_it_should_return_an_error() {
+    let input = "item1,abc,20,3.0";
+    let result = ItemTemplate::from_str(input);
+    assert!(result.is_err());
+}
+
+#[test]
+fn when_parsing_an_item_input_with_an_invalid_item_weight_it_should_return_an_error() {
+    let input = "item1,10.5,20,xyz";
+    let result = ItemTemplate::from_str(input);
+    assert!(result.is_err());
+}
+
+#[test]
+fn when_parsing_an_item_input_with_an_invalid_item_amount_it_should_return_an_error() {
+    let input = "item1,10.5,abc,3.0";
+    let result = ItemTemplate::from_str(input);
+    assert!(result.is_err());
+}
+
+// PackTemplate
+
+#[test]
+fn when_creating_a_new_pack_template_it_should_initialize_properly() {
+    let pack = PackTemplate::new();
+    assert_eq!(pack.maximum_number_of_pieces, 0);
+    assert_eq!(pack.maximum_weight, 0.0);
+    assert_eq!(pack.sort_order, PackSortOrder::NotSet);
+}
+
+#[test]
+fn when_parsing_a_valid_pack_input_string_it_should_return_a_pack_template() {
+    let mut pack = PackTemplate::new();
+    let input = "NATURAL,10,20.0";
+    let result = pack.from_line(input);
+    assert!(result.is_ok());
+    assert_eq!(pack.maximum_number_of_pieces, 10);
+    assert_eq!(pack.maximum_weight, 20.0);
+    assert_eq!(pack.sort_order, PackSortOrder::Natural);
+}
+
+#[test]
+fn when_parsing_a_pack_input_with_too_few_properties_it_should_return_an_error() {
+    let mut pack = PackTemplate::new();
+    let input = "NATURAL,10";
+    let result = pack.from_line(input);
+    assert!(result.is_err());
+    assert_eq!(
+        result.err().unwrap(),
+        Error::InvalidNumberOfPropertiesForPacks {
+            input: input.to_string(),
+            property_count: 2
+        }
+    );
+}
+
+#[test]
+fn when_parsing_a_pack_input_with_too_many_properties_it_should_return_an_error() {
+    let mut pack = PackTemplate::new();
+    let input = "NATURAL,10,20.0,Extra";
+    let result = pack.from_line(input);
+    assert!(result.is_err());
+    assert_eq!(
+        result.err().unwrap(),
+        Error::InvalidNumberOfPropertiesForPacks {
+            input: input.to_string(),
+            property_count: 4
+        }
+    );
+}
+
+#[test]
+fn when_parsing_a_pack_input_with_an_invalid_sort_order_it_should_return_an_error() {
+    let mut pack = PackTemplate::new();
+    let input = "InvalidSortOrder,10,20.0";
+    let result = pack.from_line(input);
+    assert!(result.is_err());
+}
+
+#[test]
+fn when_parsing_a_pack_input_with_an_invalid_quantity_it_should_return_an_error() {
+    let mut pack = PackTemplate::new();
+    let input = "NATURAL,abc,20.0";
+    let result = pack.from_line(input);
+    assert!(result.is_err());
+}
+
+#[test]
+fn when_parsing_a_pack_input_with_an_invalid_weight_it_should_return_an_error() {
+    let mut pack = PackTemplate::new();
+    let input = "NATURAL,10,abc";
+    let result = pack.from_line(input);
+    assert!(result.is_err());
+}
+
+// parse_input()
+
+#[test]
+fn test_parse_input_valid() {
+    let input = "NATURAL,10,20.0\n100,10.5,20,3.0\n110,8.0,15,5.0";
+    let mut cursor = Cursor::new(input);
+    let result = parse_input(&mut cursor);
+    assert!(result.is_ok());
+
+    let (pack_template, item_templates) = result.unwrap();
+    assert_eq!(pack_template.maximum_number_of_pieces, 10);
+    assert_eq!(pack_template.maximum_weight, 20.0);
+    assert_eq!(pack_template.sort_order, PackSortOrder::Natural);
+    assert_eq!(item_templates.len(), 2);
+    assert_eq!(item_templates[0].id, "100");
+    assert_eq!(item_templates[1].id, "110");
+}
+
+#[test]
+fn test_parse_input_invalid_pack_information() {
+    let input = "INVALID_KEYWORD,10,20.0\n100,10.5,20,3.0";
+    let mut cursor = Cursor::new(input);
+    let result = parse_input(&mut cursor);
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_parse_input_duplicate_pack_information() {
+    let input = "NATURAL,10,20.0\nNATURAL,8,15.0\n100,10.5,20,3.0";
+    let mut cursor = Cursor::new(input);
+    let result = parse_input(&mut cursor);
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_parse_input_invalid_item_format() {
+    let input = "NATURAL,10,20.0\ninvalid_item_format\n100,10.5,20,3.0";
+    let mut cursor = Cursor::new(input);
+    let result = parse_input(&mut cursor);
+    assert!(result.is_err());
+}
